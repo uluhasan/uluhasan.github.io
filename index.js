b32118a47d44d0e9d34a31ed3c6101aefef4161f
Hasan Ulu <117313855+uluhasan@users.noreply.github.com>
2024-12-26 18:13:16 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 document.addEventListener("contextmenu", function(e) {
     e.preventDefault();
 }, false);
+
+document.addEventListener("keydown", (e) => {
+    if (e.key === "F12" || e.keyCode === 123) {
+        e.preventDefault();
+    }
+    if (e.ctrlKey && e.shiftKey && e.key === "I") {
+        e.preventDefault();
+    }
+    if (e.ctrlKey && e.shiftKey && e.key === "J") {
+        e.preventDefault();
+    }
+    if (e.ctrlKey && e.key === "u") {
+        e.preventDefault();
+    }
+}, false);
+
