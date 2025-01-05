f5481cc4608102a78937b09c531b49843eb2d191
Hasan Ulu <117313855+uluhasan@users.noreply.github.com>
2025-01-05 13:52:16 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+(function () {
+  if (window.location.protocol !== 'https:') {
+    window.location.href = 'https://' + window.location.hostname + window.location.pathname + window.location.search;
+  }
+})();
+document.addEventListener("keydown", (e) => e.preventDefault());
 document.addEventListener("contextmenu", (e) => e.preventDefault());
-document.addEventListener("keydown", (e) => {
-    if (e.key === "F12" || (e.ctrlKey && (e.key === "u" || e.key === "U" || e.shiftKey && (e.key === "I" || e.key === "J")))) {
-        e.preventDefault();
-    }
-}, false);
+document.addEventListener("selectstart", (e) => e.preventDefault());
