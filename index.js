e9c175e00f272f26dcd1b0db9ab2f5ea40ad9380
Hasan Ulu <117313855+uluhasan@users.noreply.github.com>
2025-01-17 00:18:14 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,2 @@
-(function () {
-  if (window.location.protocol !== 'https:' && !sessionStorage.getItem('protocolRedirected')) {
-    sessionStorage.setItem('protocolRedirected', 'true');
-    const newUrl = `https://${window.location.hostname}${window.location.pathname}${window.location.search}${window.location.hash}`;
-    window.location.href = newUrl;
-  }
-})();
 document.addEventListener("keydown", (e) => e.preventDefault());
 document.addEventListener("contextmenu", (e) => e.preventDefault());
