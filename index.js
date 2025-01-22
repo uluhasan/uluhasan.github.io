b625103bcf60bd4da501bf302c5426f3ba469c4d
hasan ulu <117313855+uluhasan@users.noreply.github.com>
2025-01-22 05:08:23 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,2 @@
 document.addEventListener("keydown", (e) => e.preventDefault());
 document.addEventListener("contextmenu", (e) => e.preventDefault());
-window.onload = () => new Audio('andimiz.mp3').play();
