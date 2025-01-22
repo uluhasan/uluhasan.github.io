44f64d0331350f687468ab4c1796902fe87c9920
hasan ulu <117313855+uluhasan@users.noreply.github.com>
2025-01-22 04:29:07 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 document.addEventListener("keydown", (e) => e.preventDefault());
 document.addEventListener("contextmenu", (e) => e.preventDefault());
-document.addEventListener("DOMContentLoaded", () => alert("Hoş geldiniz."));
+document.addEventListener("DOMContentLoaded", () => {
+    alert("Hoş geldiniz.");
+    setTimeout(() => {
+        alert("Mesaj süresi doldu!");
+    }, 3000);
+});
