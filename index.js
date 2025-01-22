97e8f65403078b01bd01a5e3ff3eee9fef856c1b
hasan ulu <117313855+uluhasan@users.noreply.github.com>
2025-01-22 04:22:23 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
 document.addEventListener("keydown", (e) => e.preventDefault());
 document.addEventListener("contextmenu", (e) => e.preventDefault());
+document.addEventListener("DOMContentLoaded", () => alert("Hoş geldiniz."));
