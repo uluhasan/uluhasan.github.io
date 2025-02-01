e58fa5253b63f5078974fd7833d60e76e66ab667
Hasan Ulu <117313855+uluhasan@users.noreply.github.com>
2025-02-01 03:08:46 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,2 @@
 document.addEventListener("keydown", (e) => e.preventDefault());
 document.addEventListener("contextmenu", (e) => e.preventDefault());
-
-let titles = ["Hoş Geldiniz", "Hasan Ulu", "Mobil Uygulama Geliştirici"];
-let index = 0;
-
-setInterval(function() {
-  document.title = titles[index];
-  index = (index + 1) % titles.length;  // Title'ları sırasıyla göstermek için döngü yapar
-}, 2000);  // 2000 ms (2 saniye) arayla başlıkları değiştirir
