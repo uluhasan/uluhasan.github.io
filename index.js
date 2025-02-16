f89a2980dae243d68bc1434c2cd5a3059baa1035
Hasan Ulu <117313855+uluhasan@users.noreply.github.com>
2025-02-16 23:20:44 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 document.addEventListener("keydown", (e) => e.preventDefault());
 document.addEventListener("contextmenu", (e) => e.preventDefault());
+document.addEventListener("visibilitychange", function() {
+  let link = document.querySelector("link[rel*='icon']");
+  if (document.hidden) {
+    if (document.title !== "Güle Güle!") {
+      document.title = "Güle Güle";
+    }
+    if (link && link.href !== "basarisiz.ico") {
+      link.href = "basarisiz.ico";
+    }
+  } else {
+    if (document.title !== "Hoş Geldin!") {
+      document.title = "Hoş Geldin!";
+    }
+    if (link && link.href !== "basarili.ico") {
+      link.href = "basarili.ico";
+    }
+  }
+});
