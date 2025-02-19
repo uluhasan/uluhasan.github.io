7950769537cfbffbc17dbfe43c0b6a909492310c
Hasan Ulu <117313855+uluhasan@users.noreply.github.com>
2025-02-20 01:01:13 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 document.addEventListener("keydown", (e) => e.preventDefault());
 document.addEventListener("contextmenu", (e) => e.preventDefault());
 document.addEventListener("visibilitychange", function() {
-    const link = document.querySelector("link[rel*='icon']");
     const isPageHidden = document.hidden;
     const title = isPageHidden ? "Güle Güle!" : "Hoş Geldin!";
     const iconHref = isPageHidden ? "basarisiz.ico" : "basarili.ico";
+
+    const oldLink = document.querySelector("link[rel='icon']");
+    oldLink.parentNode.removeChild(oldLink);
+
+    const link = document.createElement("link");
+    link.rel = "icon";
+    link.type = "image/x-icon";
+    link.href = iconHref + "?v=" + new Date().getTime();
+    document.head.appendChild(link);
+
     document.title = title;
-    link.type = 'image/x-icon';
-    link.rel = 'icon';
-    link.href = iconHref;
 });
