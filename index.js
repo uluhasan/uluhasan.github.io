ec2add1142c6cbbee76881db143ae459fb950fde
Hasan Ulu <117313855+uluhasan@users.noreply.github.com>
2025-02-26 16:02:45 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,2 @@
 document.addEventListener("keydown", (e) => e.preventDefault());
 document.addEventListener("contextmenu", (e) => e.preventDefault());
-let titles = ["Hasan Ulu", "Yazılım Geliştirici"]; // Başlıkları buraya ekledik
-let index = 0;
-function changeTitle() {
-    document.title = titles[index];
-    index = (index + 1) % titles.length;
-}
-setInterval(changeTitle, 3000);
