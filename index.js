60c2db4ee4ebeeac858bac13002f60eda14127fb
Hasan Ulu <117313855+uluhasan@users.noreply.github.com>
2025-03-05 04:55:07 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,2 @@
 document.addEventListener("keydown", (e) => e.preventDefault());
 document.addEventListener("contextmenu", (e) => e.preventDefault());
-document.addEventListener("DOMContentLoaded", () => {
-    const titles = [
-      "HASAN ULU",
-      "YAZILIM GELİŞTİRİCİ",
-      "BİLGİSAYAR MÜHENDİSİ"
-    ];
-    let index = 0;
-    function changeTitle() {
-      document.title = titles[index];
-      index = (index + 1) % titles.length;
-    }
-    setInterval(changeTitle, 2000);
-});
-
