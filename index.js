6b0be4be07f8087923af1e9f8433ca835f8e2c05
uluhasan <117313855+uluhasan@users.noreply.github.com>
2025-03-09 17:20:39 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
 document.addEventListener("keydown", (e) => e.preventDefault());
 document.addEventListener("contextmenu", (e) => e.preventDefault());
+document.addEventListener("selectstart", (e) => e.preventDefault());
